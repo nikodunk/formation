5e59649b150291c082bbb7dbbc7d9f484750981e
Nikolai <n.dunkel@gmail.com>
2018-12-09 20:00:22 -0800

    heroku

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const os = require('os');
-const PORT = process.env.PORT || 8080
+
 const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
-app.listen(PORT, () => console.log('Listening on port 8080!'))
+app.listen(8080, () => console.log('Listening on port 8080!'))
