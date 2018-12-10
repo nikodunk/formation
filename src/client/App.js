976ac02a286655b7f0347bba90b41a5d6a50d6a3
Nikolai <n.dunkel@gmail.com>
2018-12-10 12:00:02 -0800

    make it better

--- src/client/App.js
+++ src/client/App.js
@@ -30,6 +30,16 @@ export default class App extends React.Component {
       <div>
         
         <div>
+          Form
+          <select>
+            <option value={'Prenatal Assessment/Reassessment and Individualized Care Plan'}>Prenatal Assessment/Reassessment and Individualized Care Plan</option>
+          </select>
+
+        </div>
+
+        <hr />
+
+        <div>
           Patient ID 
           <input
               placeholder={'Y-9147d'}
@@ -66,15 +76,15 @@ export default class App extends React.Component {
           </select>
         </div>
 
-        <div style={{paddingTop: 20}}>
-          <form id="myForm" method="post" action="/api/pdf" >
-            <input type="hidden" value={this.state.uid} name="uid" />
-            <input type="hidden" value={this.state.name} name="name" />
-            <input type="hidden" value={this.state.age} name="age" />
-            <input type="hidden" value={this.state.stayinthisarea} name="stayinthisarea" />
-            <input type="submit" value="Generate PDF Form" />
-          </form>
-        </div>
+        
+        <form style={{width: '100%'}} id="myForm" method="post" action="/api/pdf" >
+          <input type="hidden" value={this.state.uid} name="uid" />
+          <input type="hidden" value={this.state.name} name="name" />
+          <input type="hidden" value={this.state.age} name="age" />
+          <input type="hidden" value={this.state.stayinthisarea} name="stayinthisarea" />
+          <input className={'primary'} type="submit" value="Generate PDF Form" />
+        </form>
+        
         
       </div>
     );
