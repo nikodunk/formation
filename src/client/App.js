92138bdad7c0914ccade287d219d6b447d98ec5a
Nikolai <n.dunkel@gmail.com>
2018-12-09 21:36:37 -0800

    make it better

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import './app.css';
 
-export default class App extends Component {
-  state = { username: null };
+export default class App extends React.Component {
+  
+  constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.state = { 
+            username: null,
+            input: 'gaggi'
+          }
+  }
+
+  handleChange(e) {
+    this.setState({input: e.target.value});
+  }
 
   componentDidMount() {
     fetch('/api/getUsername')
@@ -10,11 +22,30 @@ export default class App extends Component {
       .then(user => this.setState({ username: user.username }));
   }
 
+
+  makePDF(){
+    // console.log(this.state.input)
+    fetch('/api/makepdf', {
+        method: 'post',
+        body: JSON.stringify({input: this.state.input}),
+        headers: { 'Content-type': 'application/json' }
+      })
+     .then(res => res.json())
+     .then(pdf => console.log(pdf))
+  }
+
   render() {
     const { username } = this.state;
     return (
       <div>
         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        <input 
+            value={this.state.input}
+            onChange={this.handleChange}
+            />
+        <button onClick={this.makePDF.bind(this)}>
+          Generate PDF Form
+        </button>
       </div>
     );
   }
--- src/server/index.js
+++ src/server/index.js
@@ -2,8 +2,25 @@ const express = require('express');
 const os = require('os');
 const PORT = process.env.PORT || 8080
 const app = express();
+const bodyParser = require("body-parser");
+const pdf = './public/prenatal.pdf'
 
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+
+
+app.get('/api/getUsername', (req, res) => {
+	res.send({ username: os.userInfo().username })
+});
+
+
+app.post('/api/makepdf', (req, res) => {
+	console.log(req.body.input)
+	res.send({ pdf: req.body.input })
+	//res.sendfile(pdf)
+});
 
 app.listen(PORT, () => console.log('Listening on port 8080!'))
