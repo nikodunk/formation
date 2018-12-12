0a124d54f152b422436052ee14d96f1c182f05b3
Nikolai <n.dunkel@gmail.com>
2018-12-12 10:03:00 -0800

    Updated branding to paperworklabs

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo Hello! Please enter a commit message
+echo -e -e "\033[0;32mHello! Please enter a commit message:\033[0m"
 read commitmessage
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
