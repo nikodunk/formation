d698898a6c132002d89ab3885c8bea5d6d9aeb2d
Nikolai <n.dunkel@gmail.com>
2018-12-19 09:31:45 -0800

    heroku deploy error: changed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
     }
   },
   plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
+    
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico'
