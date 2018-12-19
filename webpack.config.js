0947a0918814d2b23d6533d5f66a2a8c393ca523
Nikolai <n.dunkel@gmail.com>
2018-12-19 09:34:49 -0800

    removed second index.html

--- webpack.config.js
+++ webpack.config.js
@@ -37,18 +37,10 @@ module.exports = {
     }
   },
   plugins: [
-    
+    new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'login',
-      template: './public/login.html'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'app',
-      template: './public/app.html'
     })
   ]
 };
