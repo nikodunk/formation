b43d958ee4fe14e66ca66fb19b1c0349b2d81c85
Nikolai <n.dunkel@gmail.com>
2019-01-02 15:02:49 -0800

    json stringified form body data

--- src/server/routes.js
+++ src/server/routes.js
@@ -27,8 +27,8 @@ router.get('/getform/:patientuid/:formName', function(req, res, next) {
 
 
 router.post('/saveform/:patientuid/:formName', function(req, res, next) {
-			console.log(req.params.patientuid, req.params.formName, req.body)
-	        client.query(`INSERT INTO forms VALUES ( '${req.params.patientuid}',  '${req.params.formName}', '${req.body}');`, (err, queryResult) => { 
+			console.log(req.params.patientuid, req.params.formName, JSON.stringify(req.body))
+	        client.query(`INSERT INTO forms VALUES ( '${req.params.patientuid}',  '${req.params.formName}', '${JSON.stringify(req.body)}');`, (err, queryResult) => { 
 	        			res.send(queryResult)
 	        		})
 	        res.send(`saved Form to user: ${req.params.patientuid}`)
