6d48d6bd5cc94f5cae17c60e73c7c51e5e2b9c39
Nikolai <n.dunkel@gmail.com>
2019-01-07 13:14:58 -0800

    fancier reports

--- src/client/ReportingDashboard.js
+++ src/client/ReportingDashboard.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
-import {Bar} from 'react-chartjs-2';
+import {Bar, Doughnut} from 'react-chartjs-2';
 
 import api from './api'
+
+import './app.css';
+
 
 
 export default class ReportingDashboard extends React.Component {
@@ -14,11 +17,27 @@ export default class ReportingDashboard extends React.Component {
             data: {
                           labels: ["June", "July", "August", "September", "October", "November", "December"],
                           datasets: [{
-                            label: "Reports on all forms",
+                            label: "Postpartum",
                             backgroundColor: 'dodgerblue',
-                            data: [12, 5, 10, 5, 2, 20, 30, 45],
+                            data: [12, 5, 10, 5, 2, 20, 30, 45]
                           }]
-                  }
+                  },
+            gestationalage: {
+                          labels: ['> 37 weeks', '< 37 weeks'],
+                          datasets: [{
+                            label: "CPSP Postpartum form: Gestational Age",
+                            backgroundColor: ['yellow', 'dodgerblue'],
+                            data: [25, 32]
+                          }]
+            },
+            deliverycomplications: {
+                          labels: ['No', 'Yes: C-Section', 'Yes: Labor too long'],
+                          datasets: [{
+                            label: "CPSP Postpartum form: Delivery complications",
+                            backgroundColor: ['green', 'blue', 'magenta'],
+                            data: [47, 12, 2]
+                          }]
+            },
           }
   }
 
@@ -110,7 +129,7 @@ export default class ReportingDashboard extends React.Component {
 
               <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-                  <h1 class="h2">Reports Dashboard</h1>
+                  <h1 class="h2">Report Dashboard</h1>
                   <div class="btn-toolbar mb-2 mb-md-0">
                     <div class="btn-group mr-2">
                       <button class="btn btn-sm btn-outline-secondary">Share</button>
@@ -124,9 +143,10 @@ export default class ReportingDashboard extends React.Component {
                 </div>
 
                 {/*<canvas class="my-4" id="myChart" width="900" height="380"></canvas>*/}
+                
                 < Bar data={this.state.data} />
 
-                <h2>Form Feedback</h2>
+                <h2>Form Types</h2>
                 <div class="table-responsive">
                   <table class="table table-striped table-sm">
                     <thead>
@@ -156,6 +176,34 @@ export default class ReportingDashboard extends React.Component {
                     </tbody>
                   </table>
                 </div>
+
+                <br />
+                <br />
+                <hr />
+                <br />
+                <br />
+
+                <div className="container">
+                  <h2>CPSP Postpartum Responses</h2>
+                  <div className="row">
+                    <div className="col-6">
+                      <h4>2. Gestational age</h4>
+                      <Doughnut data={this.state.gestationalage} />
+                    </div>
+                    <div className="col-6">
+                      <h4>3. Delivery Complications</h4>
+                      <Doughnut data={this.state.deliverycomplications} />
+                    </div>
+                  </div>
+                </div>
+
+
+                <br />
+                <br />
+                <hr />
+                <br />
+                <br />
+
               </main>
             </div>
           </div>
