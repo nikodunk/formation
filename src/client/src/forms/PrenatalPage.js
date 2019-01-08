af004b0203159def7542eaed85de2415871ca9a8
Nikolai <n.dunkel@gmail.com>
2019-01-07 20:10:21 -0800

    no css anywher

--- src/client/src/forms/PrenatalPage.js
+++ src/client/src/forms/PrenatalPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../app.css';
+// import '../app.css';
 
 import InputRadio from '../inputComponents/InputRadio'
 import InputRadioWithText from '../inputComponents/InputRadioWithText'
--- src/client/src/inputComponents/InputRadio.js
+++ src/client/src/inputComponents/InputRadio.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../app.css';
+// import '../app.css';
 
 export default class InputRadio extends React.Component {
   
