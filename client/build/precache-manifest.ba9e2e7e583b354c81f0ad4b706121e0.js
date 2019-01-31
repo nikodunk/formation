a09be189b40c551d26ce9b78b1b9420c49916c21
Nikolai <n.dunkel@gmail.com>
2019-01-31 13:58:28 -0800

    ASD

---
+++ client/build/precache-manifest.ba9e2e7e583b354c81f0ad4b706121e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bab235802ac4fc1c924f",
+    "url": "/static/js/main.bab23580.chunk.js"
+  },
+  {
+    "revision": "864f420e61c31f3e0e8b",
+    "url": "/static/js/1.864f420e.chunk.js"
+  },
+  {
+    "revision": "bab235802ac4fc1c924f",
+    "url": "/static/css/main.f07898bc.chunk.css"
+  },
+  {
+    "revision": "b855fe097c6750b11066756f5743015a",
+    "url": "/index.html"
+  }
+];
