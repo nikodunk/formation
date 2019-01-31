1a9a55686e6d8d84da61fd8f15d994226609fc52
Nikolai <n.dunkel@gmail.com>
2019-01-31 13:35:12 -0800

    workers

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,14 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/paperworklabs',
+    connection: {
+        host : process.env.RDS_HOSTNAME, 
+        user : process.env.RDS_USERNAME,
+        password : process.env.RDS_PASSWORD,
+        database : process.env.RDS_DB_NAME,
+        port: process.env.RDS_PORT
+      },
+    // connection: 'postgres://localhost/paperworklabs',
     ssl: true,
     seeds: {
       directory: './db/seeds/'
