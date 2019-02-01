e0d2dfae73ce252966079377e93dbb538169779c
Nikolai <n.dunkel@gmail.com>
2019-02-01 00:36:37 -0800

    trying longer timeout

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ console.log('bisi')
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
 
-server.on('connection', function(socket) {
+app.on('connection', function(socket) {
   console.log("A new connection was made by a client.");
   socket.setTimeout(30 * 1000); 
   // 30 second timeout. Change this as you see fit.
