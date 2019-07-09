b1b72d1f68713e6b0ee4ca531844fbd6fa76afa3
Nikolai <nikolai@nikolais-air.lan>
2019-07-09 11:29:59 -0700

    working

---
+++ paperworklabs-firebase-adminsdk-7dyaq-bf9a61493e.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "type": "service_account",
+  "project_id": "paperworklabs",
+  "private_key_id": "bf9a61493ee5beb3bfc1bc99a762b8bbb454d1da",
+  "private_key": process.env.FIREBASE_KEY,
+  "client_email": "firebase-adminsdk-7dyaq@paperworklabs.iam.gserviceaccount.com",
+  "client_id": "102029500346944047489",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7dyaq%40paperworklabs.iam.gserviceaccount.com"
+}
