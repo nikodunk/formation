2c19e837baeae86aaf573952691f360dd89d0525
Nikolai <nikolai@nikolais-air.lan>
2019-07-09 11:53:34 -0700

    working with double escape of config var

--- paperworklabs-firebase-adminsdk-7dyaq-bf9a61493e.js
+++ paperworklabs-firebase-adminsdk-7dyaq-bf9a61493e.js
@@ -2,7 +2,7 @@ module.exports = {
   "type": "service_account",
   "project_id": "paperworklabs",
   "private_key_id": "bf9a61493ee5beb3bfc1bc99a762b8bbb454d1da",
-  "private_key": process.env.FIREBASE_KEY,
+  "private_key": process.env.FIREBASE_KEY.replace(/\\n/g, '\n'),
   "client_email": "firebase-adminsdk-7dyaq@paperworklabs.iam.gserviceaccount.com",
   "client_id": "102029500346944047489",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
